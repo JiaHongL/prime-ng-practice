e5145fe66af7bf49c3785cc0e044ccc413b13064
joe <>
2023-01-06 21:14:10 +0800

    練習 treeSelect

--- src/app/pages/forms/tree-select/tree-select.component.ts
+++ src/app/pages/forms/tree-select/tree-select.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NodeService } from 'src/app/services/nodeservice';
 
 @Component({
   selector: 'app-tree-select',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tree-select.component.scss']
 })
 export class TreeSelectComponent implements OnInit {
+  nodes1!: any[];
 
-  constructor() { }
+  nodes2!: any[];
 
-  ngOnInit(): void {
+  nodes3!: any[];
+
+  nodes4!: any[];
+
+  selectedNodes1: any[] = [];
+
+  selectedNodes2: any[] = [];
+
+  selectedNodes3: any[] = [];
+
+  selectedNode: any;
+
+  constructor(public nodeService: NodeService) { }
+
+  ngOnInit() {
+      this.nodeService.getFiles().then(files => this.nodes1 = files);
+      this.nodeService.getFiles().then(files => this.nodes2 = files);
+      this.nodeService.getFiles().then(files => this.nodes3 = files);
+      this.nodeService.getFiles().then(files => this.nodes4 = files);
   }
-
 }
