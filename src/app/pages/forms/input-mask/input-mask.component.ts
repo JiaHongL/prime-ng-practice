8ff698119fa547c5b700e4080d05a11ffa617d33
joe <>
2023-01-06 10:24:39 +0800

    練習 input-mask

--- src/app/pages/forms/input-mask/input-mask.component.ts
+++ src/app/pages/forms/input-mask/input-mask.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputMaskComponent implements OnInit {
 
+  val1!: string;
+
+  val2!: string;
+
+  val3!: string;
+
+  val4!: string;
+
+  val5!: string;
+  
+  val6!: string;
+
   constructor() { }
 
   ngOnInit(): void {
