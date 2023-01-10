edce10c50c8ba7791d424754f685968d96c3ef3b
joe <>
2023-01-10 16:38:32 +0800

    練習 table dynamic

--- src/app/pages/data/table/dynamic/dynamic.component.ts
+++ src/app/pages/data/table/dynamic/dynamic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product, ProductService } from 'src/app/services/product.service';
 
 @Component({
   templateUrl: './dynamic.component.html',
@@ -6,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DynamicComponent implements OnInit {
 
-  constructor() { }
+  products!: Product[];
 
-  ngOnInit(): void {
+  cols!: any[];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+      this.productService.getProductsSmall().then(data => this.products = data);
+
+      this.cols = [
+          { field: 'code', header: 'Code' },
+          { field: 'name', header: 'Name' },
+          { field: 'category', header: 'Category' },
+          { field: 'quantity', header: 'Quantity' }
+      ];
   }
 
 }
