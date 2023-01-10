d86acb7bbeefdf640b1f2efb9f422ac5e8d048e0
joe <>
2023-01-10 17:54:46 +0800

    練習 table lazy

--- src/app/pages/data/table/lazy/lazy.component.ts
+++ src/app/pages/data/table/lazy/lazy.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LazyLoadEvent, PrimeNGConfig } from 'primeng/api';
+import { Customer, CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-lazy',
@@ -6,10 +8,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lazy.component.scss']
 })
 export class LazyComponent implements OnInit {
+  datasource!: Customer[];
 
-  constructor() { }
+  customers!: Customer[];
 
-  ngOnInit(): void {
+  totalRecords!: number;
+
+  cols!: any[];
+
+  loading!: boolean;
+
+  constructor(private customerService: CustomerService, private primengConfig: PrimeNGConfig) { }
+
+  ngOnInit() {
+    //datasource imitation
+    this.customerService.getCustomersLarge().then(data => {
+      this.datasource = data;
+      this.totalRecords = data.length;
+    });
+
+    this.loading = true;
+    this.primengConfig.ripple = true;
+  }
+
+  loadCustomers(event: LazyLoadEvent) {
+
+    console.log('event', event);
+
+    this.loading = true;
+
+    //in a real application, make a remote request to load data using state metadata from event
+    //event.first = First row offset
+    //event.rows = Number of rows per page
+    //event.sortField = Field name to sort with
+    //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
+    //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
+
+    //imitate db connection over a network
+    setTimeout(() => {
+      if (this.datasource) {
+        this.customers = this.datasource.slice(event.first, ((event?.first || 0) + (event?.rows || 0)));
+        this.loading = false;
+      }
+    }, 1000);
+
   }
 
 }
