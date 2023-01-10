ec12b890625edf7ff8c263f98b0eaafaab0d1639
joe <>
2023-01-10 20:59:28 +0800

    練習 table export

--- src/app/pages/data/table/export/export.component.ts
+++ src/app/pages/data/table/export/export.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product, ProductService } from 'src/app/services/product.service';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-export',
@@ -6,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./export.component.scss']
 })
 export class ExportComponent implements OnInit {
+  products!: Product[];
 
-  constructor() { }
+  selectedProducts!: Product[];
 
-  ngOnInit(): void {
+  constructor(private productService: ProductService) { }
+
+  cols!: any[];
+
+  exportColumns!: any[];
+
+  ngOnInit() {
+    this.productService.getProductsSmall().then(data => (this.products = data));
+
+    this.cols = [
+      { field: "code", header: "Code" },
+      { field: "name", header: "Name" },
+      { field: "category", header: "Category" },
+      { field: "quantity", header: "Quantity" }
+    ];
+
+    this.exportColumns = this.cols.map(col => ({
+      title: col.header,
+      dataKey: col.field
+    }));
   }
 
+  exportExcel() {
+    import("xlsx").then(xlsx => {
+      const worksheet = xlsx.utils.json_to_sheet(this.products);
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ["data"] };
+      const excelBuffer: any = xlsx.write(workbook, {
+        bookType: "xlsx",
+        type: "array"
+      });
+      this.saveAsExcelFile(excelBuffer, "products");
+    });
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    let EXCEL_TYPE =
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+    let EXCEL_EXTENSION = ".xlsx";
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+    });
+    saveAs(
+      data,
+      fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION
+    );
+  }
 }
