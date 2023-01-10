9057d55a663d5fa51a5b7c992a6abf4d5deefd48
joe <>
2023-01-10 18:50:38 +0800

    練習 table edit

--- src/app/pages/data/table/edit/edit.component.ts
+++ src/app/pages/data/table/edit/edit.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService, SelectItem } from 'primeng/api';
+import { Product, ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.scss']
+  styleUrls: ['./edit.component.scss'],
+  providers: [MessageService],
+  styles: [`
+      :host ::ng-deep .p-cell-editing {
+          padding-top: 0 !important;
+          padding-bottom: 0 !important;
+      }
+  `]
 })
 export class EditComponent implements OnInit {
+  products1!: Product[];
 
-  constructor() { }
+  products2!: Product[];
 
-  ngOnInit(): void {
+  statuses!: SelectItem[];
+
+  clonedProducts: { [s: string]: Product; } = {};
+
+  constructor(private productService: ProductService, private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.productService.getProductsSmall().then(data => this.products1 = data);
+    this.productService.getProductsSmall().then(data => this.products2 = data);
+
+    this.statuses = [{ label: 'In Stock', value: 'INSTOCK' }, { label: 'Low Stock', value: 'LOWSTOCK' }, { label: 'Out of Stock', value: 'OUTOFSTOCK' }]
+  }
+
+  onRowEditInit(product: Product) {
+    if (product?.id) {
+      this.clonedProducts[product.id] = { ...product };
+    }
+  }
+
+  onRowEditSave(product: Product) {
+
+    if (product.price && product.price > 0) {
+      delete this.clonedProducts[product.id];
+      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Product is updated' });
+    }
+    else {
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Invalid Price' });
+    }
+  }
+
+  onRowEditCancel(product: Product, index: number) {
+
+    this.products2[index] = this.clonedProducts[product.id];
+
+    delete this.products2[ (product.id as any)];
+
   }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 export interface Product {
-    id?: string;
+    id: string;
     code?: string;
     name?: string;
     description?: string;
