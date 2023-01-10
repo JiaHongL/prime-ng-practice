290b5826888b73af75a962f52c05f399e198c932
joe <>
2023-01-10 20:48:49 +0800

    練習 table responsive

--- src/app/pages/data/table/responsive/responsive.component.ts
+++ src/app/pages/data/table/responsive/responsive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product, ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-responsive',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResponsiveComponent implements OnInit {
 
-  constructor() { }
+  products!: Product[];
 
-  ngOnInit(): void {
+  cols!: any[];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+      this.productService.getProductsSmall().then(data => this.products = data);
+
+      this.cols = [
+          { field: 'code', header: 'Code' },
+          { field: 'name', header: 'Name' },
+          { field: 'category', header: 'Category' },
+          { field: 'quantity', header: 'Quantity' },
+          { field: 'inventoryStatus', header: 'Status' },
+          { field: 'rating', header: 'Rating' }
+      ];
   }
-
+  
 }
