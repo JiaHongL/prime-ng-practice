25e08c464ca8edf02a7fd78a922994320c4a37d2
joe <>
2023-01-10 17:02:47 +0800

    練習 table page

--- src/app/pages/data/table/page/page.component.ts
+++ src/app/pages/data/table/page/page.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer, CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss']
 })
-export class PageComponent implements OnInit {
+export class PageComponent {
+  customers!: Customer[];
 
-  constructor() { }
+  first = 0;
 
-  ngOnInit(): void {
+  rows = 10;
+
+  constructor(private customerService: CustomerService) { }
+
+  ngOnInit() {
+    this.customerService.getCustomersLarge().then(customers => this.customers = customers);
   }
 
+  next() {
+    this.first = this.first + this.rows;
+  }
+
+  prev() {
+    this.first = this.first - this.rows;
+  }
+
+  reset() {
+    this.first = 0;
+  }
+
+  isLastPage(): boolean {
+    return this.customers ? this.first === (this.customers.length - this.rows) : true;
+  }
+
+  isFirstPage(): boolean {
+    return this.customers ? this.first === 0 : true;
+  }
 }
