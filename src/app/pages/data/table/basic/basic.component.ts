a8f8786b6142c28095284b7e82c5f2026f967f07
joe <>
2023-01-10 16:33:20 +0800

    練習 table basic

--- src/app/pages/data/table/basic/basic.component.ts
+++ src/app/pages/data/table/basic/basic.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Product, ProductService } from 'src/app/services/product.service';
 
 @Component({
   templateUrl: './basic.component.html',
   styleUrls: ['./basic.component.scss']
 })
 export class BasicComponent implements OnInit {
+  products!: Product[];
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+      this.productService.getProductsSmall().then(data => this.products = data);
   }
 
 }
