8e3a047d801dbcf457d57c404d1796090761e865
joe <>
2023-01-11 23:18:24 +0800

    練習 tree-table responsive

--- src/app/pages/data/tree-table/responsive/responsive.component.ts
+++ src/app/pages/data/tree-table/responsive/responsive.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { TreeNode } from 'primeng/api';
+import { NodeService } from 'src/app/services/node.service';
 
 @Component({
   selector: 'app-responsive',
   templateUrl: './responsive.component.html',
-  styleUrls: ['./responsive.component.scss']
+  styleUrls: ['./responsive.component.scss'],
+  styles: [`
+    :host ::ng-deep .priority-2, 
+    :host ::ng-deep .priority-3,
+    :host ::ng-deep .visibility-sm {
+        display: none;
+    }
+
+    @media screen and (max-width: 39.938em) {
+        :host ::ng-deep .visibility-sm {
+            display: inline;
+        }
+    }
+
+    @media screen and (min-width: 40em) {
+        :host ::ng-deep .priority-2 {
+            display: table-cell;
+        }
+    }
+
+    @media screen and (min-width: 64em) {
+        :host ::ng-deep .priority-3 {
+            display: table-cell;
+        }
+    }
+  `]
 })
 export class ResponsiveComponent implements OnInit {
 
-  constructor() { }
+  files!: TreeNode[];
 
-  ngOnInit(): void {
+  cols!: any[];
+
+  constructor(private nodeService: NodeService) { }
+
+  ngOnInit() {
+      this.nodeService.getFilesystem().then(files => this.files = files);
+
+      this.cols = [
+          { field: 'name', header: 'Name' },
+          { field: 'size', header: 'Size' },
+          { field: 'type', header: 'Type' }
+      ];
   }
-
+  
 }
