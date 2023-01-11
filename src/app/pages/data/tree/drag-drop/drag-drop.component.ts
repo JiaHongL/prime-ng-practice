c23f16e16382d70b43dfa6e5c257fcd96ab110a8
joe <>
2023-01-11 19:58:26 +0800

    練習 Tree DragDrop

--- src/app/pages/data/tree/drag-drop/drag-drop.component.ts
+++ src/app/pages/data/tree/drag-drop/drag-drop.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService, TreeDragDropService, TreeNode } from 'primeng/api';
+import { NodeService } from 'src/app/services/node.service';
 
 @Component({
   selector: 'app-drag-drop',
   templateUrl: './drag-drop.component.html',
-  styleUrls: ['./drag-drop.component.scss']
+  styleUrls: ['./drag-drop.component.scss'],
+  providers: [TreeDragDropService, MessageService],
+  styles: [`
+      h4 {
+          text-align: center;
+          margin: 0 0 8px 0;
+      }
+  `]
 })
-export class DragDropComponent implements OnInit {
+export class DragDropComponent implements OnInit { 
+  files1!: TreeNode[];
 
-  constructor() { }
+  files2!: TreeNode[];
 
-  ngOnInit(): void {
+  files3!: TreeNode[];
+
+  files4!: TreeNode[];
+  
+  constructor(private nodeService: NodeService) { }
+
+  ngOnInit() {
+      this.nodeService.getFiles().then(files => this.files1 = files);
+      this.nodeService.getFiles().then(files => this.files2 = files);
+      this.files3 = [{
+              label: "Backup",
+              data: "Backup Folder",
+              expandedIcon: "pi pi-folder-open",
+              collapsedIcon: "pi pi-folder"
+          }
+      ];
+
+      this.files4 = [{
+              label: "Storage",
+              data: "Storage Folder",
+              expandedIcon: "pi pi-folder-open",
+              collapsedIcon: "pi pi-folder"
+          }
+      ];
   }
-
 }
