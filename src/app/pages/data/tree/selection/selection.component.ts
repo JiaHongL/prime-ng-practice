f96510c6d188fc292534b52b281f06052c6b0ee9
joe <>
2023-01-11 17:16:38 +0800

    練習 tree selection

--- src/app/pages/data/tree/selection/selection.component.ts
+++ src/app/pages/data/tree/selection/selection.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService, TreeNode } from 'primeng/api';
+import { NodeService } from 'src/app/services/node.service';
 
 @Component({
   selector: 'app-selection',
   templateUrl: './selection.component.html',
-  styleUrls: ['./selection.component.scss']
+  styleUrls: ['./selection.component.scss'],
+  providers: [MessageService]
 })
 export class SelectionComponent implements OnInit {
+  files1!: TreeNode[];
 
-  constructor() { }
+  files2!: TreeNode[];
 
-  ngOnInit(): void {
+  files3!: TreeNode[];
+
+  selectedFile!: TreeNode;
+
+  selectedFiles1!: TreeNode;
+
+  selectedFiles2!: TreeNode;
+
+  constructor(
+    private nodeService: NodeService,
+    private messageService: MessageService
+  ) { }
+
+  ngOnInit() {
+    this.nodeService.getFiles().then((files) => (this.files1 = files));
+    this.nodeService.getFiles().then((files) => (this.files2 = files));
+    this.nodeService.getFiles().then((files) => (this.files3 = files));
   }
 
+  nodeSelect(event: any) {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Node Selected',
+      detail: event.node.label,
+    });
+  }
+
+  nodeUnselect(event: any) {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Node Unselected',
+      detail: event.node.label,
+    });
+  }
+  
 }
