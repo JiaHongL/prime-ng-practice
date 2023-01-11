9c1a3d3ab17bf83357af47812084f6730e590b15
joe <>
2023-01-11 23:27:15 +0800

    練習 tree-table size

--- src/app/pages/data/tree-table/size/size.component.ts
+++ src/app/pages/data/tree-table/size/size.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TreeNode } from 'primeng/api';
+
+import { NodeService } from 'src/app/services/node.service';
+
 @Component({
   selector: 'app-size',
   templateUrl: './size.component.html',
   styleUrls: ['./size.component.scss']
 })
 export class SizeComponent implements OnInit {
+  files1!: TreeNode[];
 
-  constructor() { }
+  files2!: TreeNode[];
 
-  ngOnInit(): void {
+  files3!: TreeNode[];
+
+  files4!: TreeNode[];
+
+  cols!: any[];
+
+  constructor(private nodeService: NodeService) {}
+
+  ngOnInit() {
+
+    this.nodeService.getFilesystem().then(files => (this.files1 = files));
+    this.nodeService.getFilesystem().then(files => (this.files2 = files));
+    this.nodeService.getFilesystem().then(files => (this.files3 = files));
+    this.nodeService.getFilesystem().then(files => (this.files4 = files));
+
+    this.cols = [
+      { field: "name", header: "Name", width: "50%" },
+      { field: "size", header: "Size", width: "30%" },
+      { field: "type", header: "Type", width: "20%" }
+    ];
+
   }
 
 }
