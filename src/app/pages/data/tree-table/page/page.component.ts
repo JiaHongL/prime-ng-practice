85e8942d5a1993c59c6048e3a62bc5c7437b7348
joe <>
2023-01-11 21:40:39 +0800

    練習 tree-table page

--- src/app/pages/data/tree-table/page/page.component.ts
+++ src/app/pages/data/tree-table/page/page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig, TreeNode } from 'primeng/api';
+import { NodeService } from 'src/app/services/node.service';
 
 @Component({
   selector: 'app-page',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page.component.scss']
 })
 export class PageComponent implements OnInit {
+  files!: TreeNode[];
 
-  constructor() { }
+  cols!: any[];
 
-  ngOnInit(): void {
+  constructor(private nodeService: NodeService, private primengConfig: PrimeNGConfig) { }
+
+  ngOnInit() {
+      this.files = [];
+      for(let i = 0; i < 50; i++) {
+          let node = {
+              data:{  
+                  name: 'Item ' + i,
+                  size: Math.floor(Math.random() * 1000) + 1 + 'kb',
+                  type: 'Type ' + i
+              },
+              children: [
+                  {
+                      data: {  
+                          name: 'Item ' + i + ' - 0',
+                          size: Math.floor(Math.random() * 1000) + 1 + 'kb',
+                          type: 'Type ' + i
+                      }
+                  }
+              ]
+          };
+
+          this.files.push(node);
+      }
+
+      this.cols = [
+          { field: 'name', header: 'Name' },
+          { field: 'size', header: 'Size' },
+          { field: 'type', header: 'Type' }
+      ];
+    this.primengConfig.ripple = true;
   }
-
 }
