b88880fe13a74110e2a39ea8bf317c841775f44b
joe <>
2023-01-11 17:39:09 +0800

    練習 tree lazy

--- src/app/pages/data/tree/lazy/lazy.component.ts
+++ src/app/pages/data/tree/lazy/lazy.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService, TreeNode } from 'primeng/api';
+
+import { NodeService } from 'src/app/services/node.service';
 
 @Component({
   selector: 'app-lazy',
   templateUrl: './lazy.component.html',
-  styleUrls: ['./lazy.component.scss']
+  styleUrls: ['./lazy.component.scss'],
+  providers: [MessageService]
 })
 export class LazyComponent implements OnInit {
+  files!: TreeNode[];
 
-  constructor() { }
+  loading!: boolean;
 
-  ngOnInit(): void {
+  constructor(private nodeService: NodeService, private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.loading = true;
+    setTimeout(() => {
+      this.nodeService.getLazyFiles().then(files => this.files = files);
+      this.loading = false;
+    }, 1000);
+  }
+
+  nodeExpand(event: any) {
+    if (event.node) {
+      //in a real application, make a call to a remote url to load children of the current node and add the new nodes as children
+      this.nodeService.getLazyFiles().then(nodes => {
+        event.node.children = nodes
+        this.messageService.add({ severity: 'info', summary: 'Children Loaded', detail: event.node.label });
+      });
+    }
   }
 
 }
