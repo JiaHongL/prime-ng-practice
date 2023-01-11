f1444648df1beb55958165e8660aec919c8abfd3
joe <>
2023-01-11 17:45:45 +0800

    練習 tree scroll

--- src/app/pages/data/tree/scroll/scroll.component.ts
+++ src/app/pages/data/tree/scroll/scroll.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { TreeNode } from 'primeng/api';
+import { NodeService } from 'src/app/services/node.service';
 
 @Component({
-  selector: 'app-scroll',
-  templateUrl: './scroll.component.html',
-  styleUrls: ['./scroll.component.scss']
+    selector: 'app-scroll',
+    templateUrl: './scroll.component.html',
+    styleUrls: ['./scroll.component.scss']
 })
 export class ScrollComponent implements OnInit {
+    files1!: TreeNode[];
 
-  constructor() { }
+    files2!: TreeNode[];
 
-  ngOnInit(): void {
-  }
+    files3!: TreeNode[];
 
+    dialogVisible!: boolean;
+
+    constructor(private nodeService: NodeService) { }
+
+    ngOnInit() {
+        this.nodeService.getFiles().then(files => this.files1 = files);
+        this.files2 = Array.from({ length: 100 }).map((_, i) => this.createNode(i, 2));
+        this.files3 = Array.from({ length: 50 }).map((_, i) => this.createNode(i, 1000));
+    }
+
+    createNode(i: number, children: number): TreeNode {
+        let node: TreeNode = {
+            label: 'Node ' + i,
+            data: 'Node ' + i,
+            expandedIcon: 'pi pi-folder-open',
+            collapsedIcon: 'pi pi-folder',
+            children: Array.from({ length: children }).map((_, j) => {
+                return {
+                    label: 'Node ' + i + '.' + j,
+                    data: 'Node ' + i + '.' + j,
+                    icon: 'pi pi-file-o'
+                }
+            })
+        };
+        return node;
+    }
+
+    showDialog() {
+        this.dialogVisible = true;
+    }
 }
