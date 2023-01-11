de679e20b19b4371ca24fb278fa7089009a45bea
joe <>
2023-01-11 21:43:21 +0800

    練習 tree-table sort

--- src/app/pages/data/tree-table/sort/sort.component.ts
+++ src/app/pages/data/tree-table/sort/sort.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TreeNode } from 'primeng/api';
+import { NodeService } from 'src/app/services/node.service';
 
 @Component({
   selector: 'app-sort',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SortComponent implements OnInit {
 
-  constructor() { }
+  files1!: TreeNode[];
 
-  ngOnInit(): void {
+  files2!: TreeNode[];
+
+  cols!: any[];
+
+  constructor(private nodeService: NodeService) { }
+
+  ngOnInit() {
+
+    this.nodeService.getFilesystem().then(files => this.files1 = files);
+    this.nodeService.getFilesystem().then(files => {
+      this.files2 = files;
+      this.files2.push({
+        data: {
+          name: 'Documents',
+          size: '100kb',
+          type: 'Link'
+        }
+      });
+    });
+
+    this.cols = [
+      { field: 'name', header: 'Name' },
+      { field: 'size', header: 'Size' },
+      { field: 'type', header: 'Type' }
+    ];
+    
   }
 
 }
