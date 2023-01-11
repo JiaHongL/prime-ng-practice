50258cf88ef7dd85c8294b3b4bf98ec449b472c7
joe <>
2023-01-11 23:07:58 +0800

    練習 tree-table style

--- src/app/pages/data/tree-table/style/style.component.ts
+++ src/app/pages/data/tree-table/style/style.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { TreeNode } from 'primeng/api';
+import { NodeService } from 'src/app/services/node.service';
 
 @Component({
   selector: 'app-style',
   templateUrl: './style.component.html',
-  styleUrls: ['./style.component.scss']
+  styleUrls: ['./style.component.scss'],
+  styles: [`
+  .kb-row {
+      background-color: rgba(0,0,0,.15) !important;
+  }
+
+  .kb-cell {
+      font-weight: 700;
+      color: #FF5252;
+      text-decoration: line-through;
+  }
+`]
 })
 export class StyleComponent implements OnInit {
 
-  constructor() { }
+  files!: TreeNode[];
 
-  ngOnInit(): void {
+  cols!: any[];
+
+  constructor(private nodeService: NodeService) { }
+
+  ngOnInit() {
+    this.nodeService.getFilesystem().then(files => this.files = files);
+
+    this.cols = [
+      { field: 'name', header: 'Name' },
+      { field: 'size', header: 'Size' },
+      { field: 'type', header: 'Type' }
+    ];
   }
-
+  
 }
+
