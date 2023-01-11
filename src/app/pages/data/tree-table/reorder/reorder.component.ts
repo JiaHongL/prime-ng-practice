4aca46b4c13b35cc6e11890467fa841d418e36b1
joe <>
2023-01-11 22:17:21 +0800

    練習 tree-table reorder

--- src/app/pages/data/tree-table/reorder/reorder.component.ts
+++ src/app/pages/data/tree-table/reorder/reorder.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { TreeNode } from 'primeng/api';
+import { NodeService } from 'src/app/services/node.service';
 
 @Component({
   selector: 'app-reorder',
   templateUrl: './reorder.component.html',
   styleUrls: ['./reorder.component.scss']
 })
-export class ReorderComponent implements OnInit {
+export class ReorderComponent implements OnInit { 
+  files!: TreeNode[];
 
-  constructor() { }
+  cols!: any[];
 
-  ngOnInit(): void {
+  constructor(private nodeService: NodeService) { }
+
+  ngOnInit() {
+      this.nodeService.getFilesystem().then(files => this.files = files);
+
+      this.cols = [
+          { field: 'name', header: 'Name' },
+          { field: 'size', header: 'Size' },
+          { field: 'type', header: 'Type' }
+      ];
   }
-
 }
