6282f1a45a7e0e9d40e63ccd672e603b9cf148ac
joe <>
2023-01-12 17:02:25 +0800

    練習 panel toolbar

--- src/app/pages/panel/toolbar/toolbar.component.ts
+++ src/app/pages/panel/toolbar/toolbar.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
+  items = [
+    {
+        label: 'Update',
+        icon: 'pi pi-refresh'
+    },
+    {
+        label: 'Delete',
+        icon: 'pi pi-times'
+    },
+    {
+        label: 'Angular',
+        icon: 'pi pi-external-link',
+        url: 'http://angular.io'
+    },
+    {
+        label: 'Router',
+        icon: 'pi pi-upload',
+        routerLink: '/fileupload'
+    }
+];
+
   constructor() { }
 
   ngOnInit(): void {
