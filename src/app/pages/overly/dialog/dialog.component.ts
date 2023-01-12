bd5d8138e7b05be8f7f15fb077d2f50fb828811b
joe <>
2023-01-12 20:30:27 +0800

    練習 overly dialog

--- src/app/pages/overly/dialog/dialog.component.ts
+++ src/app/pages/overly/dialog/dialog.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
-  styleUrls: ['./dialog.component.scss']
+  styleUrls: ['./dialog.component.scss'],
+  styles: [`
+  :host ::ng-deep .p-button {
+      margin: 0 .5rem 0 0;
+      min-width: 10rem;
+  }
+
+  p {
+      margin: 0;
+  }
+
+  .confirmation-content {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+  }
+
+  :host ::ng-deep .p-dialog .p-button {
+      min-width: 6rem;
+  }
+`]
 })
-export class DialogComponent implements OnInit {
+export class DialogComponent {
 
-  constructor() { }
+  constructor(private primengConfig: PrimeNGConfig) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+
+  displayModal!: boolean;
+
+  displayBasic!: boolean;
+
+  displayBasic2!: boolean;
+
+  displayMaximizable!: boolean;
+
+  displayPosition!: boolean;
+
+  position!: string;
+
+  displayResponsive!: boolean;
+
+  showModalDialog() {
+    this.displayModal = true;
+  }
+
+  showBasicDialog() {
+    this.displayBasic = true;
+  }
+
+  showBasicDialog2() {
+    this.displayBasic2 = true;
+  }
+
+  showMaximizableDialog() {
+    this.displayMaximizable = true;
+  }
+
+  showPositionDialog(position: string) {
+    this.position = position;
+    this.displayPosition = true;
+  }
+
+  showResponsiveDialog() {
+    this.displayResponsive = true;
   }
 
 }
