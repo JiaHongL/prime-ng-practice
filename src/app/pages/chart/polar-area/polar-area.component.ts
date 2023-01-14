4c9da1f1b5738f5f9829a7d21d0ee88dceaa1c4c
joe <>
2023-01-14 16:53:23 +0800

    練習 polar area

--- src/app/pages/chart/polar-area/polar-area.component.ts
+++ src/app/pages/chart/polar-area/polar-area.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AppConfig, AppConfigService } from 'src/app/services/app-config.service';
 
 @Component({
   selector: 'app-polar-area',
@@ -7,9 +9,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PolarAreaComponent implements OnInit {
 
-  constructor() { }
+  data: any;
 
-  ngOnInit(): void {
+  chartOptions: any;
+
+  subscription!: Subscription;
+
+  config!: AppConfig;
+
+  constructor(private configService: AppConfigService) {}
+
+  ngOnInit() {
+      this.data = {
+          datasets: [{
+              data: [
+                  11,
+                  16,
+                  7,
+                  3,
+                  14
+              ],
+              backgroundColor: [
+                  "#42A5F5",
+                  "#66BB6A",
+                  "#FFA726",
+                  "#26C6DA",
+                  "#7E57C2"
+              ],
+              label: 'My dataset'
+          }],
+          labels: [
+              "Red",
+              "Green",
+              "Yellow",
+              "Grey",
+              "Blue"
+          ]
+      };
+
+      this.config = this.configService.config;
+      // this.updateChartOptions();
+      // this.subscription = this.configService.configUpdate$.subscribe(config => {
+      //     this.config = config;
+      //     this.updateChartOptions();
+      // });
   }
 
+  updateChartOptions() {
+      this.chartOptions = this.config && this.config.dark ? this.getDarkTheme() : this.getLightTheme();
+  }
+
+  getLightTheme() {
+      return {
+          plugins: {
+              legend: {
+                  labels: {
+                      color: '#495057'
+                  }
+              }
+          },
+          scales: {
+              r: {
+                  grid: {
+                      color: '#ebedef'
+                  }
+              }
+          }
+      }
+  }
+
+  getDarkTheme() {
+      return {
+          plugins: {
+              legend: {
+                  labels: {
+                      color: '#ebedef'
+                  }
+              }
+          },
+          scales: {
+              r: {
+                  grid: {
+                      color: 'rgba(255,255,255,0.2)'
+                  }
+              }
+          }
+      }
+  }
 }
