765f116c40310cdedda4bafba19279261015a11f
joe <>
2023-01-14 17:38:48 +0800

    練習 message

--- src/app/pages/messages/messages.module.ts
+++ src/app/pages/messages/messages.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { PrimeNgModule } from 'src/app/primeng.module';
 
 import { MessagesRoutingModule } from './messages-routing.module';
 import { MessagesComponent } from './messages/messages.component';
 import { ToastComponent } from './toast/toast.component';
-
 
 @NgModule({
   declarations: [
@@ -13,7 +15,10 @@ import { ToastComponent } from './toast/toast.component';
   ],
   imports: [
     CommonModule,
-    MessagesRoutingModule
+    MessagesRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PrimeNgModule
   ]
 })
 export class MessagesModule { }
--- src/app/pages/messages/messages/messages.component.ts
+++ src/app/pages/messages/messages/messages.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Message, MessageService, PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss']
+  styleUrls: ['./messages.component.scss'],
+  providers: [MessageService]
 })
 export class MessagesComponent implements OnInit {
+  msgs1!: Message[];
 
-  constructor() { }
+  msgs2!: Message[];
 
-  ngOnInit(): void {
+  constructor(private messageService: MessageService, private primengConfig: PrimeNGConfig) { }
+
+  ngOnInit() {
+    this.msgs1 = [
+      { severity: 'success', summary: 'Success', detail: 'Message Content' },
+      { severity: 'info', summary: 'Info', detail: 'Message Content' },
+      { severity: 'warn', summary: 'Warning', detail: 'Message Content' },
+      { severity: 'error', summary: 'Error', detail: 'Message Content' }
+    ];
+
+    this.primengConfig.ripple = true;
   }
+
+  addMessages() {
+    this.msgs2 = [
+      { severity: 'success', summary: 'Success', detail: 'Message Content' },
+      { severity: 'info', summary: 'Info', detail: 'Message Content' },
+      { severity: 'warn', summary: 'Warning', detail: 'Message Content' },
+      { severity: 'error', summary: 'Error', detail: 'Message Content' }
+    ];
+  }
+
+  clearMessages() {
+    this.msgs2 = [];
+  }
+
+  showViaService() {
+    this.messageService.add({ severity: 'success', summary: 'Service Message', detail: 'Via MessageService' });
+  }
+
+  addMultiple() {
+    this.messageService.addAll([{ severity: 'success', summary: 'Service Message', detail: 'Via MessageService' },
+    { severity: 'info', summary: 'Info Message', detail: 'Via MessageService' }]);
+  }
+
 
 }
