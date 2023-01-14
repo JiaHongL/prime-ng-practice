6783134851d73ba423e49f72e0bac4fdf589ece9
joe <>
2023-01-14 17:02:05 +0800

    練習 pie chart 圓餅圖

--- src/app/pages/chart/pie/pie.component.ts
+++ src/app/pages/chart/pie/pie.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AppConfig, AppConfigService } from 'src/app/services/app-config.service';
+
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-pie',
   templateUrl: './pie.component.html',
@@ -7,9 +11,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PieComponent implements OnInit {
 
-  constructor() { }
+  data: any;
 
-  ngOnInit(): void {
+  chartOptions: any;
+
+  subscription!: Subscription;
+
+  config!: AppConfig;
+
+  constructor(private configService: AppConfigService) {}
+
+  ngOnInit() {
+      this.data = {
+          labels: ['A','B','C'],
+          datasets: [
+              {
+                  data: [300, 50, 100],
+                  backgroundColor: [
+                      "#42A5F5",
+                      "#66BB6A",
+                      "#FFA726"
+                  ],
+                  hoverBackgroundColor: [
+                      "#64B5F6",
+                      "#81C784",
+                      "#FFB74D"
+                  ]
+              }
+          ]
+      };
+
+      this.config = this.configService.config;
+      // this.updateChartOptions();
+      // this.subscription = this.configService.configUpdate$.subscribe(config => {
+      //     this.config = config;
+      //     this.updateChartOptions();
+      // });
+  }
+
+  updateChartOptions() {
+      this.chartOptions = this.config && this.config.dark ? this.getDarkTheme() : this.getLightTheme();
+  }
+
+  getLightTheme() {
+      return {
+          plugins: {
+              legend: {
+                  labels: {
+                      color: '#495057'
+                  }
+              }
+          }
+      }
+  }
+
+  getDarkTheme() {
+      return {
+          plugins: {
+              legend: {
+                  labels: {
+                      color: '#ebedef'
+                  }
+              }
+          }
+      }
   }
 
 }
+
