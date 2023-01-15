69bec30991750077430271d0395a73e7d7c6818a
joe <>
2023-01-15 17:20:31 +0800

    練習 galleria fullscreen

--- src/app/pages/media/galleria/fullscreen/fullscreen.component.ts
+++ src/app/pages/media/galleria/fullscreen/fullscreen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoService } from 'src/app/services/photo.service';
 
 @Component({
   selector: 'app-fullscreen',
@@ -6,10 +7,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fullscreen.component.scss']
 })
 export class FullscreenComponent implements OnInit {
+  images!: any[];
 
-  constructor() { }
+  responsiveOptions:any[] = [
+      {
+          breakpoint: '1024px',
+          numVisible: 5
+      },
+      {
+          breakpoint: '768px',
+          numVisible: 3
+      },
+      {
+          breakpoint: '560px',
+          numVisible: 1
+      }
+  ];
 
-  ngOnInit(): void {
+  responsiveOptions2:any[] = [
+      {
+          breakpoint: '1500px',
+          numVisible: 5
+      },
+      {
+          breakpoint: '1024px',
+          numVisible: 3
+      },
+      {
+          breakpoint: '768px',
+          numVisible: 2
+      },
+      {
+          breakpoint: '560px',
+          numVisible: 1
+      }
+  ];
+
+  displayBasic!: boolean;
+
+  displayBasic2!: boolean;
+
+  displayCustom!: boolean;
+
+  activeIndex: number = 0;
+
+  constructor(private photoService: PhotoService) { }
+
+  ngOnInit() {
+      this.photoService.getImages().then(images =>{
+          this.images = images
+      })
   }
 
+  imageClick(index: number) {
+      this.activeIndex = index;
+      this.displayCustom = true;
+  }
 }
