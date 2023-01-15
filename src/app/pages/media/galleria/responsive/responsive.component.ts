5cfd896086e5fb0165acdcbad9cc9c673402fa3f
joe <>
2023-01-15 17:16:06 +0800

    練習 galleria Responsive

--- src/app/pages/media/galleria/responsive/responsive.component.ts
+++ src/app/pages/media/galleria/responsive/responsive.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoService } from 'src/app/services/photo.service';
 
 @Component({
   selector: 'app-responsive',
   templateUrl: './responsive.component.html',
   styleUrls: ['./responsive.component.scss']
 })
-export class ResponsiveComponent implements OnInit {
+export class ResponsiveComponent implements OnInit  {
+  images!: any[];
 
-  constructor() { }
+  responsiveOptions:any[] = [
+      {
+          breakpoint: '1024px',
+          numVisible: 5
+      },
+      {
+          breakpoint: '960px',
+          numVisible: 4
+      },
+      {
+          breakpoint: '768px',
+          numVisible: 3
+      },
+      {
+          breakpoint: '560px',
+          numVisible: 1
+      }
+  ];
 
-  ngOnInit(): void {
+  constructor(private photoService: PhotoService) { }
+
+  ngOnInit() {
+      this.photoService.getImages().then(images =>{ 
+          this.images = images
+      })
   }
-
+  
 }
