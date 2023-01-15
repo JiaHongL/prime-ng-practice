44e54679779821768fb10a0248c6ab72f42074c6
joe <>
2023-01-15 16:58:03 +0800

    練習 galleria programmatic

--- src/app/pages/media/galleria/programmatic/programmatic.component.ts
+++ src/app/pages/media/galleria/programmatic/programmatic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoService } from 'src/app/services/photo.service';
 
 @Component({
   selector: 'app-programmatic',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./programmatic.component.scss']
 })
 export class ProgrammaticComponent implements OnInit {
+  images!: any[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get activeIndex(): number {
+    return this._activeIndex;
   }
 
+  set activeIndex(newValue) {
+    if (this.images && 0 <= newValue && newValue <= this.images.length - 1) {
+      this._activeIndex = newValue;
+    }
+  }
+
+  _activeIndex: number = 2;
+
+  responsiveOptions: any[] = [
+    {
+      breakpoint: '1024px',
+      numVisible: 5
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 3
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1
+    }
+  ];
+
+  constructor(private photoService: PhotoService) { }
+
+  ngOnInit() {
+    this.photoService.getImages().then((images) => (this.images = images));
+  }
+
+  next() {
+    this.activeIndex++;
+  }
+
+  prev() {
+    this.activeIndex--;
+  }
 }
