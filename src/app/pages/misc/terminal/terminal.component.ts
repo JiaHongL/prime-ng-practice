629182c1dfc3cb532cf417b647f49cfb9d1bb725
joe <>
2023-01-16 18:52:19 +0800

    練習 misc terminal

--- src/app/pages/misc/terminal/terminal.component.ts
+++ src/app/pages/misc/terminal/terminal.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TerminalService } from 'primeng/terminal';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-terminal',
   templateUrl: './terminal.component.html',
-  styleUrls: ['./terminal.component.scss']
+  styleUrls: ['./terminal.component.scss'],
+  providers: [TerminalService]
 })
-export class TerminalComponent implements OnInit {
+export class TerminalComponent {
+  subscription!: Subscription;
 
-  constructor() { }
+  constructor(private terminalService: TerminalService) {
+    this.terminalService.commandHandler.subscribe(command => {
+      let response = (command === 'date') ? new Date().toDateString() : 'Unknown command: ' + command;
+      this.terminalService.sendResponse(response);
+    });
+  }
 
-  ngOnInit(): void {
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
 }
