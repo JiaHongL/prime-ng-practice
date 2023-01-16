d09608b5fac0e544333463c878f2cd9aa34426ad
joe <>
2023-01-16 18:46:33 +0800

    練習 misc Skeleton

--- src/app/pages/misc/skeleton/skeleton.component.ts
+++ src/app/pages/misc/skeleton/skeleton.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkeletonComponent implements OnInit {
 
+  products = [{},{},{},{},{}]
+
   constructor() { }
 
   ngOnInit(): void {
